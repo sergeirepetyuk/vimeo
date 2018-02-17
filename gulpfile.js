0a0b5f464bee332f0a414fc35d1da44ef7235aef
sergeirepetyuk <sergeirepetyuk@gmail.com>
2018-02-17 20:10:13 +0200

    compilation gulp project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var fs = require("fs");
 var {promisify} = require("util");
+var browserSync = require('browser-sync').create();
 const readFile = promisify(fs.readFile);
 
 gulp.task('default', ['less', "mustache"], function() {
   gulp.watch('./src/styles/**/*.less', ['less']);
+  gulp.watch("./src/images/*.*", ["copy"]);
   gulp.watch([
-    './src/data/db.json',
+    './src/data/data.json',
     './src/**/*.html'
   ], ['mustache']);
 });
@@ -19,15 +21,27 @@ gulp.task('less', function () {
   .pipe(less({
     paths: [ path.join(__dirname, 'less', 'includes') ]
   }))
-  .pipe(gulp.dest('./docs/css'));
+  .pipe(gulp.dest('./docs/styles'));
 });
 
 var mustache = require("gulp-mustache");
 gulp.task("mustache", async () => {
-  const data = JSON.parse(await readFile("./src/data/db.json", "utf-8"));
+  const data = JSON.parse(await readFile("./src/data/data.json", "utf-8"));
 
   return gulp.src("./src/**/*.html")
   .pipe(mustache(data))
   .pipe(gulp.dest("./docs"));
 })
 
+gulp.task('copy', function () {
+    gulp.src('./src/images/*.*')
+        .pipe(gulp.dest('./docs/images/'));
+});
+gulp.task('server', function(){
+    browserSync.init({
+        server:{
+            baseDir: "docs/"
+        },
+        files: ['docs/*.html','docs/*.styles','docs/images/*.*']
+    });
+});
